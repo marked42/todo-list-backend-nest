7da63d7be11f722ab271abbb6c7cfaa69544e644
penghui <penghui@penghuideMac-mini.local>
2025-06-29 23:07:16 +0800

    feat: role entity

--- src/core/CoreModule.ts
+++ src/core/CoreModule.ts
@@ -3,10 +3,11 @@ import { UserController } from './controller/UserController';
 import { UserService } from './service/UserService';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/User';
+import { Role } from './entity/Role';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Role])],
   controllers: [UserController],
   providers: [UserService],
   exports: [],
--- src/core/entity/User.ts
+++ src/core/entity/User.ts
@@ -2,9 +2,12 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinTable,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Role } from './Role';
 
 @Entity()
 export class User {
@@ -15,6 +18,18 @@ export class User {
     unique: true,
   })
   username: string;
+
+  @OneToMany(() => Role, (role: Role) => role.code)
+  @JoinTable({
+    joinColumns: [{ name: 'user_id' }],
+    inverseJoinColumns: [{ name: 'role_id' }],
+  })
+  roles: string[];
+
+  @Column({
+    name: 'encrypted_password',
+  })
+  encryptedPassword: string;
 
   @Column({
     default: false,
