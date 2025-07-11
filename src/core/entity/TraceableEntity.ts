15049d25caf3845a6fa447ff525e925d7b1a632e
penghui <penghui@penghuideMac-mini.local>
2025-07-11 23:22:07 +0800

    refactor(service): use relation id in entity definition
    load all entities in lazy way and use relation entity id

--- src/core/entity/TraceableEntity.ts
+++ src/core/entity/TraceableEntity.ts
@@ -1,13 +1,15 @@
-import { Entity, ManyToOne, ManyToMany, JoinColumn } from 'typeorm';
+import { Entity, ManyToOne, ManyToMany, RelationId } from 'typeorm';
 import { User } from './User';
 import { BaseEntity } from './BaseEntity';
 
 @Entity()
 export class TraceableEntity extends BaseEntity {
   @ManyToOne(() => User, { nullable: false })
-  @JoinColumn({ name: 'created_by_user_id' }) // 自定义外键字段名
-  createdBy: User;
+  creator: User;
+
+  @RelationId((entity: TraceableEntity) => entity.creator)
+  creatorId: number;
 
   @ManyToMany(() => User)
-  updatedBy: User[];
+  updaters: User[];
 }
