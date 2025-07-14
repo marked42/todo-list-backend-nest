6e1bfd417f41ff5916061db78d8b14ddeb6b63c4
zhangpenghui <zhangpenghui@kuaishou.com>
2025-07-14 10:27:57 +0800

    refactor(test): simplify controller test
    controller test should not repeat every service case, only need to test
    if service is properly called

--- src/todo/controller/TaskController.spec.ts
+++ src/todo/controller/TaskController.spec.ts
@@ -1,19 +1,16 @@
 import { Test } from '@nestjs/testing';
-import { ForbiddenException, NotFoundException } from '@nestjs/common';
-import { User } from '@/core/entity/User';
+import { NotFoundException } from '@nestjs/common';
 import { TaskController } from './TaskController';
 import { TaskService } from '../service/TaskService';
 import { Task } from '../entity/Task';
 import { TaskMoveResult } from '../model';
 import { TaskMoveRequest } from '../dto/TaskMoveRequest';
-import { Repository } from 'typeorm';
-import { TaskList } from '../entity/TaskList';
+import { TaskCreateRequest } from '../dto/TaskCreateRequest';
+import { TaskUpdateRequest } from '../dto/TaskUpdateRequest';
 
 describe('TaskController', () => {
   let controller: TaskController;
   let taskService: TaskService;
-
-  const mockUser = { id: 1, name: 'Test' } as User;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -21,11 +18,7 @@ describe('TaskController', () => {
       providers: [
         {
           provide: TaskService,
-          useValue: new TaskService(
-            {} as Repository<TaskList>,
-            {} as Repository<Task>,
-            mockUser,
-          ),
+          useValue: Object.create(TaskService.prototype),
         },
       ],
     }).compile();
@@ -41,16 +34,15 @@ describe('TaskController', () => {
   describe('createTask', () => {
     it('should create a task', async () => {
       const taskListId = 1;
-      const mockTaskCreateRequest = {
-        name: 'New Task',
-        content: '',
-        taskListId,
-      };
+      const request = new TaskCreateRequest();
+      request.name = 'New Task';
+      request.content = '';
+      request.taskListId = taskListId;
 
       const mockTask = {
         id: 1,
-        name: mockTaskCreateRequest.name,
-        content: mockTaskCreateRequest.content,
+        name: request.name,
+        content: request.content,
         taskList: { id: taskListId },
       } as Task;
 
@@ -58,9 +50,26 @@ describe('TaskController', () => {
         .spyOn(taskService, 'createTask')
         .mockResolvedValue(mockTask);
 
-      const result = await controller.createTask(mockTaskCreateRequest);
-      expect(createTaskSpy).toHaveBeenCalledWith(mockTaskCreateRequest);
+      const result = await controller.createTask(request);
+      expect(createTaskSpy).toHaveBeenCalledWith(request);
       expect(result).toEqual(mockTask);
+    });
+
+    it('should propagate exception if creation failed', async () => {
+      const taskListId = 1;
+      const request = new TaskCreateRequest();
+      request.name = 'New Task';
+      request.content = '';
+      request.taskListId = taskListId;
+
+      const createTaskSpy = jest
+        .spyOn(taskService, 'createTask')
+        .mockRejectedValue(new Error('Creation failed'));
+
+      await expect(controller.createTask(request)).rejects.toThrow(
+        'Creation failed',
+      );
+      expect(createTaskSpy).toHaveBeenCalledWith(request);
     });
   });
 
@@ -80,10 +89,10 @@ describe('TaskController', () => {
       });
     });
 
-    it('should throw NotFoundException if task does not exist', async () => {
+    it('should propagate exception if failed', async () => {
       const taskId = -1;
 
-      jest
+      const deleteTaskSpy = jest
         .spyOn(taskService, 'deleteTask')
         .mockRejectedValue(
           new NotFoundException(`Task with ID ${taskId} not found`),
@@ -92,77 +101,46 @@ describe('TaskController', () => {
       await expect(controller.deleteTask(taskId)).rejects.toThrow(
         `Task with ID ${taskId} not found`,
       );
-    });
-
-    it('should throw ForbiddenException if task does not belong to user', async () => {
-      const taskId = 1;
-      const userId = 2;
-
-      jest
-        .spyOn(taskService, 'deleteTask')
-        .mockRejectedValue(
-          new ForbiddenException(
-            `Task with ID ${taskId} not owned by user ${userId}`,
-          ),
-        );
-
-      await expect(controller.deleteTask(taskId)).rejects.toThrow(
-        `Task with ID ${taskId} not owned by user ${userId}`,
-      );
+      expect(deleteTaskSpy).toHaveBeenCalledWith(taskId);
     });
   });
 
   describe('updateTask', () => {
     it('should update a task', async () => {
       const taskId = 1;
-      const updateData = { name: 'Updated Task' };
+      const request = new TaskUpdateRequest();
+      request.name = 'Updated Task';
 
       const updateTaskSpy = jest
         .spyOn(taskService, 'updateTask')
         .mockResolvedValue({
           id: taskId,
-          ...updateData,
+          ...request,
         } as Task);
 
-      const result = await controller.updateTask(taskId, updateData);
-      expect(updateTaskSpy).toHaveBeenCalledWith(taskId, updateData);
+      const result = await controller.updateTask(taskId, request);
+      expect(updateTaskSpy).toHaveBeenCalledWith(taskId, request);
       expect(result).toEqual({
         success: true,
         message: `Task with ID ${taskId} updated successfully`,
       });
     });
 
-    it('should throw NotFoundException if task does not exist', async () => {
+    it('should propagate exception if update failed', async () => {
       const taskId = -1;
-      const updateData = { name: 'Updated Task' };
+      const request = new TaskUpdateRequest();
+      request.name = 'Updated Task';
 
-      jest
+      const updateTaskSpy = jest
         .spyOn(taskService, 'updateTask')
         .mockRejectedValue(
           new NotFoundException(`Task with ID ${taskId} not found`),
         );
 
-      await expect(controller.updateTask(taskId, updateData)).rejects.toThrow(
+      await expect(controller.updateTask(taskId, request)).rejects.toThrow(
         `Task with ID ${taskId} not found`,
       );
-    });
-
-    it('should throw ForbiddenException if task does not belong to user', async () => {
-      const taskId = 1;
-      const userId = 2;
-      const updateData = { name: 'Updated Task' };
-
-      jest
-        .spyOn(taskService, 'updateTask')
-        .mockRejectedValue(
-          new ForbiddenException(
-            `Task with ID ${taskId} not owned by user ${userId}`,
-          ),
-        );
-
-      await expect(controller.updateTask(taskId, updateData)).rejects.toThrow(
-        `Task with ID ${taskId} not owned by user ${userId}`,
-      );
+      expect(updateTaskSpy).toHaveBeenCalledWith(taskId, request);
     });
   });
 
@@ -173,104 +151,50 @@ describe('TaskController', () => {
       const moveTaskSpy = jest
         .spyOn(taskService, 'moveTask')
         .mockResolvedValue(TaskMoveResult.Success);
-      const moveRequest = new TaskMoveRequest();
-      moveRequest.taskListId = 3;
+      const request = new TaskMoveRequest();
+      request.taskListId = 3;
 
-      const result = await controller.moveTask(taskId, moveRequest);
-      expect(moveTaskSpy).toHaveBeenCalledWith(taskId, moveRequest);
+      const result = await controller.moveTask(taskId, request);
+      expect(moveTaskSpy).toHaveBeenCalledWith(taskId, request);
       expect(result).toEqual({
         success: true,
-        message: `Task with ID ${taskId} moved to task list ${moveRequest.taskListId} successfully`,
+        message: `Task with ID ${taskId} moved to task list ${request.taskListId} successfully`,
       });
     });
 
     it('should return successfully if task is already in the target task list', async () => {
       const taskId = 1;
-      const moveRequest = new TaskMoveRequest();
+      const request = new TaskMoveRequest();
       // Same as current task list id
-      moveRequest.taskListId = 1;
+      request.taskListId = 1;
 
-      jest
+      const moveTaskSpy = jest
         .spyOn(taskService, 'moveTask')
         .mockResolvedValue(TaskMoveResult.AlreadyInPlace);
 
-      const result = await controller.moveTask(taskId, moveRequest);
+      const result = await controller.moveTask(taskId, request);
+      expect(moveTaskSpy).toHaveBeenCalledWith(taskId, request);
       expect(result).toEqual({
         success: true,
-        message: `Task with ID ${taskId} is already in task list ${moveRequest.taskListId}`,
+        message: `Task with ID ${taskId} is already in task list ${request.taskListId}`,
       });
     });
 
-    it('should throw NotFoundException if task does not exist', async () => {
+    it('should propagate exception if failed', async () => {
       const taskId = -1;
-      const moveRequest = new TaskMoveRequest();
-      moveRequest.taskListId = 3;
+      const request = new TaskMoveRequest();
+      request.taskListId = 3;
 
-      jest
+      const moveTaskSpy = jest
         .spyOn(taskService, 'moveTask')
         .mockRejectedValue(
           new NotFoundException(`Task with ID ${taskId} not found`),
         );
 
-      await expect(controller.moveTask(taskId, moveRequest)).rejects.toThrow(
+      await expect(controller.moveTask(taskId, request)).rejects.toThrow(
         `Task with ID ${taskId} not found`,
       );
-    });
-
-    it('should throw ForbiddenException if task does not belong to user', async () => {
-      const taskId = 1;
-      const userId = 2;
-      const moveRequest = new TaskMoveRequest();
-      moveRequest.taskListId = 3;
-
-      jest
-        .spyOn(taskService, 'moveTask')
-        .mockRejectedValue(
-          new ForbiddenException(
-            `Task with ID ${taskId} not owned by user ${userId}`,
-          ),
-        );
-
-      await expect(controller.moveTask(taskId, moveRequest)).rejects.toThrow(
-        `Task with ID ${taskId} not owned by user ${userId}`,
-      );
-    });
-
-    it('should throw NotFoundException if target task list does not exist', async () => {
-      const taskId = 1;
-      const moveRequest = new TaskMoveRequest();
-      moveRequest.taskListId = -1;
-
-      jest
-        .spyOn(taskService, 'moveTask')
-        .mockRejectedValue(
-          new NotFoundException(
-            `Task list with ID ${moveRequest.taskListId} not found`,
-          ),
-        );
-
-      await expect(controller.moveTask(taskId, moveRequest)).rejects.toThrow(
-        `Task list with ID ${moveRequest.taskListId} not found`,
-      );
-    });
-
-    it('should throw ForbiddenException if user does not have permission to move task', async () => {
-      const taskId = 1;
-      const userId = 2;
-      const moveRequest = new TaskMoveRequest();
-      moveRequest.taskListId = 3;
-
-      jest
-        .spyOn(taskService, 'moveTask')
-        .mockRejectedValue(
-          new ForbiddenException(
-            `User ${userId} does not have permission to move task with ID ${taskId}`,
-          ),
-        );
-
-      await expect(controller.moveTask(taskId, moveRequest)).rejects.toThrow(
-        `User ${userId} does not have permission to move task with ID ${taskId}`,
-      );
+      expect(moveTaskSpy).toHaveBeenCalledWith(taskId, request);
     });
   });
 });
