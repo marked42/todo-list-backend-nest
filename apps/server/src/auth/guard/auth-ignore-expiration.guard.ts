20e68f6a4c26a43ec5baf567bdf6b57da2cfd450
penghui <penghui@penghuideMac-mini.local>
2025-08-01 21:24:53 +0800

    feat: supports using Public decorator to pass JwtAuthGuard

--- apps/server/src/auth/guard/auth-ignore-expiration.guard.ts
+++ apps/server/src/auth/guard/auth-ignore-expiration.guard.ts
@@ -1,4 +1,7 @@
+import { ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { IS_PUBLIC_KEY } from '../decorator/public.decorator';
 
 // This guard ignores the expiration of JWT tokens
 // It can be used for routes where you want to allow access even if the token is expired
@@ -7,6 +10,21 @@ export class JwtIgnoreExpirationAuthGuard extends AuthGuard(
 ) {}
 
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
   // This guard uses the standard JWT strategy which checks for token expiration
   // It can be used for routes where you want to enforce token validity
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
 }
