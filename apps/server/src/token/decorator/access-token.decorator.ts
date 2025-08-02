f2a2714664d872a9f8aa4065aa6d67c1969e0f7f
penghui <penghui@penghuideMac-mini.local>
2025-08-02 21:33:57 +0800

    fix: parameter decorator signature
    missing first parameter

--- apps/server/src/token/decorator/access-token.decorator.ts
+++ apps/server/src/token/decorator/access-token.decorator.ts
@@ -5,12 +5,14 @@ import {
 } from '@nestjs/common';
 import { accessTokenExtractor } from '../extractor/access-token.extractor';
 
-export const AccessToken = createParamDecorator((ctx: ExecutionContext) => {
-  // Assuming the user is stored in the request object
-  const request = ctx.switchToHttp().getRequest<Request>();
-  const token = accessTokenExtractor(request);
-  if (!token) {
-    throw new UnauthorizedException('User not authenticated');
-  }
-  return token;
-});
+export const AccessToken = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    // Assuming the user is stored in the request object
+    const request = ctx.switchToHttp().getRequest<Request>();
+    const token = accessTokenExtractor(request);
+    if (!token) {
+      throw new UnauthorizedException('User not authenticated');
+    }
+    return token;
+  },
+);
