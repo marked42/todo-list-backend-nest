bcd12b7cfed80136c93eb15a042903bf0edbf907
penghui <penghui@penghuideMac-mini.local>
2025-08-02 21:16:24 +0800

    fix: access token service config and auth service refresh token verify logic

--- apps/server/src/token/config/access-token.config.ts
+++ apps/server/src/token/config/access-token.config.ts
@@ -3,10 +3,12 @@ import { registerAs } from '@nestjs/config';
 export const AccessTokenConfig = registerAs('accessToken', () => {
   return {
     jwtModuleOptions: {
+      signOptions: {
+        audience: process.env.JWT_TOKEN_AUDIENCE,
+        issuer: process.env.JWT_TOKEN_ISSUER,
+        expiresIn: process.env.JWT_ACCESS_TOKEN_TTL || '1h',
+      },
       secret: process.env.JWT_ACCESS_TOKEN_SECRET || 'default_secret',
-      audience: process.env.JWT_TOKEN_AUDIENCE,
-      issuer: process.env.JWT_TOKEN_ISSUER,
-      expiresIn: process.env.JWT_ACCESS_TOKEN_TTL || '1h',
     },
   };
 });
